bd01993d2b189d3abe0897bf61636cc59058c315
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-11-15 10:33:39 +0100

    Spring 1-2 3 1/2

---
+++ T3P1/src/main/java/org/example/harrypotter/controllers/Houses.java
@@ -0,0 +1,30 @@
+package org.example.harrypotter.controllers;
+
+import org.example.harrypotter.entities.House;
+import org.example.harrypotter.repositories.HouseRepository;
+import org.example.harrypotter.services.HouseService;
+import org.example.harrypotter.services.HouseServiceImplementation;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class Houses {
+    private HouseService houses = new HouseServiceImplementation(new HouseRepository());
+
+    @GetMapping("/houses")
+    public String houses(Model model) {
+        List<House> houseList = houses.getHouses();
+        model.addAttribute("houses", houseList);
+        return "Houses-Hogwarts";
+    }
+
+    @GetMapping("/houseD/{name}")
+    public String getHouse(Model model, @PathVariable String name) {
+        model.addAttribute("house",houses.getHouseByName(name));
+        return "HouseDetalled";
+    }
+}
---
+++ T3P1/src/main/java/org/example/harrypotter/controllers/Students.java
@@ -0,0 +1,36 @@
+package org.example.harrypotter.controllers;
+
+import org.example.harrypotter.entities.Student;
+import org.example.harrypotter.repositories.StudentRepository;
+import org.example.harrypotter.services.StudentService;
+import org.example.harrypotter.services.StudentServiceImplementation;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class Students {
+    private StudentService student = new StudentServiceImplementation(new StudentRepository());
+
+    @GetMapping("/house/{houseStudent}")
+    public String houseStudent(@PathVariable String houseStudent, Model model) {
+        model.addAttribute("students", student.getStudentsByHouse(houseStudent));
+        return "Students-House";
+    }
+
+    @GetMapping("/houseStudent")
+    public String studentRequestedPram(@RequestParam String name, Model model) {
+        model.addAttribute("students", student.getStudentByName(name));
+        return "StudentDetalled";
+    }
+
+    @GetMapping("/StudentD/{name}")
+    public String getHouse(Model model, @PathVariable String name) {
+        model.addAttribute("student",student.getStudentByName(name));
+        return "StudentDetalled";
+    }
+}
