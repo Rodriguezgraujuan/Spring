22e3fd32ea87187ee9c9cf898568b194a4cb3aba
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-11-22 10:03:24 +0100

    Start

---
+++ T3P3/src/main/java/practicajrg/t3p3/T3P3Application.java
@@ -0,0 +1,13 @@
+package practicajrg.t3p3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3P3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T3P3Application.class, args);
+    }
+
+}
---
+++ T3P3/src/test/java/practicajrg/t3p3/T3P3ApplicationTests.java
@@ -0,0 +1,13 @@
+package practicajrg.t3p3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T3P3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
