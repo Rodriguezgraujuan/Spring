28d9a2b4db9c853826b6ce8e8ea12f49ee525603
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2024-12-05 13:10:40 +0100

    Fin falta trabajadores(tareas)

--- T3P3/src/main/java/practicajrg/t3p3/controllers/MainController.java
+++ T3P3/src/main/java/practicajrg/t3p3/controllers/MainController.java
@@ -3,12 +3,18 @@ package practicajrg.t3p3.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import practicajrg.t3p3.entities.Tarea;
+import practicajrg.t3p3.entities.Trabajador;
 import practicajrg.t3p3.services.EquipoService;
 import practicajrg.t3p3.services.TareaService;
 import practicajrg.t3p3.services.TrabajadorService;
 
 import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 public class MainController {
@@ -29,7 +35,16 @@ public class MainController {
     }
 
     @GetMapping("/mando")
-    public String cuadroMando(){
+    public String cuadroMando(Model model){
+        model.addAttribute("trabajador", new Trabajador());
+        model.addAttribute("trabajadores", trabajadorService.findAll());
+        List<Tarea> listaTareasOrdenada = new java.util.ArrayList<>(tareaService.findAll().stream().filter(p -> p.getStatus().equals(Tarea.Status.Abierta)).toList());
+        listaTareasOrdenada.sort(Comparator.comparing(Tarea::getStartDate));
+        model.addAttribute("tareasAbiertas", listaTareasOrdenada);
+        model.addAttribute("tareasBug", tareaService.findAll().stream().filter(p->p.getType().equals(Tarea.Type.Bug)).toList());
+        model.addAttribute("tareasMejora", tareaService.findAll().stream().filter(p->p.getType().equals(Tarea.Type.Mejora)).toList());
+        model.addAttribute("tareasRefactorizacion", tareaService.findAll().stream().filter(p->p.getType().equals(Tarea.Type.Refactorizacion)).toList());
+
         return "cuadroDeMando";
     }
 
