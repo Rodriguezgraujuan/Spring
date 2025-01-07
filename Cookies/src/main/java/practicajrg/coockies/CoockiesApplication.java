aac9d4c8983cc897cb5b1171a06d97656524fac6
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2025-01-07 13:04:39 +0100

    Cookies

---
+++ Cookies/src/main/java/practicajrg/coockies/CoockiesApplication.java
@@ -0,0 +1,13 @@
+package practicajrg.coockies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoockiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoockiesApplication.class, args);
+    }
+
+}
---
+++ Cookies/src/test/java/practicajrg/coockies/CoockiesApplicationTests.java
@@ -0,0 +1,13 @@
+package practicajrg.coockies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoockiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
