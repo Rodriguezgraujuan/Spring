41b780253610204396cb5973ec8206c284fcbaae
rodriguezgraujuan <rodriguezgraujuan@gmail.com>
2025-01-09 08:29:01 +0100

    Login T4P2

---
+++ T4_P2/src/main/java/practicajrg/t4_p2/T4P2Application.java
@@ -0,0 +1,13 @@
+package practicajrg.t4_p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T4P2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T4P2Application.class, args);
+    }
+
+}
---
+++ T4_P2/src/test/java/practicajrg/t4_p2/T4P2ApplicationTests.java
@@ -0,0 +1,13 @@
+package practicajrg.t4_p2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T4P2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
